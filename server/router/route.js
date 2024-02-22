f2832a0efbce2107beb879015f835c2682180c83
Becky Weeks <becky.weeks@panerabread.com>
2024-02-22 15:48:27 -0500

    generated email

--- server/router/route.js
+++ server/router/route.js
@@ -4,10 +4,11 @@ const router = Router();
 /** import all controllers */
 import * as controller from '../controllers/appController.js'
 import Auth, { localVariables } from '../middleware/auth.js'
+import { registerMail } from '../controllers/mailer.js'
 
 /** POST Method */
 router.route('/register').post(controller.register); // register user
-// router.route('/registerMail').post(); //send the email
+router.route('/registerMail').post(registerMail); //send the email
 router.route('/authenticate').post((req, res) => res.end()); // authenticate user
 router.route('/login').post(controller.verifyUser, controller.login); // login in app
 
