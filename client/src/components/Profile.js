04070d93bf743b9ca9d97295f735b09190889f85
Becky Weeks <becky.weeks@panerabread.com>
2024-02-24 13:03:05 -0500

    protected routes

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,10 +1,12 @@
 import React, {useState} from 'react'
-import { Link } from 'react-router-dom'
 import avatar from '../assets/profile.png'
-import { Toaster } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 import { useFormik } from 'formik'
 import { profileValidation } from '../helper/validate'
 import convertToBase64 from '../helper/convert'
+import useFetch from '../hooks/fetch.hook';
+import { updateUser } from '../helper/helper'
+import { useNavigate } from 'react-router-dom'
 
 import styles from '../styles/Username.module.css'
 import extend from '../styles/Profile.module.css'
@@ -12,21 +14,31 @@ import extend from '../styles/Profile.module.css'
 export default function Profile() {
 
   const [file, setFile ] = useState()
+  const [{ isLoading, apiData, serverError }] = useFetch();
+  const navigate = useNavigate();
 
   const formik = useFormik({
     initialValues : {
-      firstName: '',
-      lastName: '',
-      email: "rsnow721@gmail.com",
-      mobile: '',
-      address : ''
+      firstName: apiData?.firstName || '',
+      lastName: apiData?.lastName || '',
+      email: apiData?.email || '',
+      mobile: apiData?.mobile || '',
+      address: apiData?.address || ''
     },
+    enableReinitialize: true,
     validate : profileValidation,
     validateOnBlur: false,
     validateOnChange: false,
     onSubmit : async values => {
-      values = await Object.assign(values, { profile : file || ''})
-      console.log(values)
+      values = await Object.assign(values, { profile : file || apiData?.profile || ''})
+      let updatePromise = updateUser(values);
+      
+      toast.promise(updatePromise, {
+        loading: 'Updating...',
+        success: <b>Update Successful...!</b>,
+        error: <b>Could not Update!</b>
+      })
+
     }
   })
 
@@ -35,6 +47,15 @@ export default function Profile() {
     const base64 = await convertToBase64(e.target.files[0]);
     setFile(base64);
   }
+
+// logout handler function
+function userLogout(){
+  localStorage.removeItem('token');
+  navigate('/');
+}
+
+  if(isLoading) return <h1 className='text-2xl font-bold'>isLoading</h1>;
+  if(serverError) return <h1 className='text-xl text-red-500'>{serverError.message}</h1>
 
     return (
     <div className='container mx-auto'>
@@ -54,7 +75,7 @@ export default function Profile() {
           <form className='py-1' onSubmit={formik.handleSubmit}>
             <div className='profile flex justify-center py-4'>
               <label htmlFor="profile">
-              <img src={file || avatar} className={`${styles.profile_img} ${extend.profile_img}`} alt='avatar' />
+              <img src={apiData?.profile || file || avatar} className={`${styles.profile_img} ${extend.profile_img}`} alt="avatar" />
               </label>
 
               <input onChange={onUpload} type="file" id='profile' name='profile'/>
@@ -76,7 +97,7 @@ export default function Profile() {
             </div>
 
             <div className='text-center py-4'>
-              <span className='text-gray-500'>Come back later? <Link className="text-red-500" to='/'>Logout</Link></span>
+              <span className='text-gray-500'>Come back later? <button onClick={userLogout} className="text-red-500" to='/'>Logout</button></span>
             </div>
           </form>
 
--- client/src/hooks/fetch.hook.js
+++ client/src/hooks/fetch.hook.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { getUsername } from '../helper/helper'
 
 axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
 
@@ -14,13 +15,14 @@ export default function useFetch(query) {
   /** custom hook */
   //eslint-disable-next-line
   useEffect(() => {
-    if (!query) return;
 
     const fetchData = async () => {
       try {
         setData(prev => ({ ...prev, isloading: true}))
 
-        const { data, status } = await axios.get(`/api/${query}`);
+        const { username } = !query ? await getUsername() : '';
+                
+        const { data, status } = !query ? await axios.get(`/api/user/${username}`) : await axios.get(`/api/${query}`);
 
         if(status === 201){
           setData(prev => ({ ...prev, isloading: false}));
